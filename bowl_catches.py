5b74fa1d7da2aea650ee3d5a8e985bbbb5353135
Oleg <viazem@gmail.com>
2020-08-11 08:46:26 +0300

    Назначение цвета фона

--- bowl_catches.py
+++ bowl_catches.py
@@ -9,12 +9,19 @@ def run_game():
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("The bowl catches the flash drive")
 
+    # Назначаем цвета фона
+    bg_color = (230, 230, 230)
+
     # Запуск основного цикла игры
     while True:
         # Отслеживани е событий клавиатуры и мыши
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # При каждом проходе цикла перерисовывается экран
+        screen.fill(bg_color)
+
         # Отображение последнего прорисованного экрана.
         pygame.display.flip()
 
